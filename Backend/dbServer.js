2638df5f901c5c2dab24fbe26db52ee356642d8c
Anfaal Mahbub <anfaal.mahbub@ucalgary.ca>
2024-02-22 14:58:03 -0700

    Update dbServer.js

--- Backend/dbServer.js
+++ Backend/dbServer.js
@@ -5,7 +5,7 @@ const db = mysql.createPool({
    connectionLimit: 100,
    host: "127.0.0.1",       //This is your localhost IP
    user: "newuser",         // "newuser" created in Step 1(e)
-   password: "password1#",  // password for the new user
+   password: "",  // password for the new user
    database: "studysyncdb",      // Database name
    port: "3306"             // port name, "3306" by default
 })
