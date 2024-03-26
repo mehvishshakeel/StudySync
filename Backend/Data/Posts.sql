b54e4cdc1048c694578b83fff767ad82fdc895df
Anfaal Mahbub <102428388+Anfaal25@users.noreply.github.com>
2024-03-26 12:04:14 -0600

    Updates to Backend SQL
    Changes to ensure updates are occurring properly

--- Backend/Data/Posts.sql
+++ Backend/Data/Posts.sql
@@ -7,9 +7,9 @@ USE Posts;
 -- Create the content table
 CREATE TABLE IF NOT EXISTS content (
   `PostID` INT NOT NULL AUTO_INCREMENT,
-  `UserID` VARCHAR(45) NULL,
-  `Title` VARCHAR(45) NULL,
-  `Content` TEXT NULL,
+  `UserID` VARCHAR(45) NOT NULL,
+  `Title` VARCHAR(45) NOT NULL,
+  `Content` TEXT NOT NULL,
   `CourseID` VARCHAR(45) NOT NULL,
   `Program`	VARCHAR(100) NULL,
   PRIMARY KEY (`PostID`));
